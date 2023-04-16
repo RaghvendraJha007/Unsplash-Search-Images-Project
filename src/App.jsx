ca731ccf56a7a2d8247992fa1f094088f7730529
RaghvendraJha007 <raghvendrajham@gmail.com>
2023-04-16 17:37:26 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Gallery from './Gallery';
+import SearchForm from './SearchForm';
+import ThemToggle from './ThemeToggle';
+
+const App = () => {
+  return (
+    <main>
+      <ThemToggle />
+      <SearchForm />
+      <Gallery />
+    </main>
+  );
+};
+export default App;
